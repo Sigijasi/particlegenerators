452ad012715c6022c550f349018f50a843b75a7c
Sigijasi <sigitas.jasiukaitis@vgtu.lt>
2018-09-05 15:37:59 +0300

    First commit

--- RandomRegular_cube.h
+++ RandomRegular_cube.h
@@ -1,1 +1,16 @@
  
+#pragma once
+#include "Generator1.h"
+
+class Random_Hexagonal_cube :public Generator1
+{
+public:
+
+    Random_Hexagonal_cube(double minimum, double maximum, int dalys);
+    ~Random_Hexagonal_cube();
+
+    double r_min, r_max;
+    int parts;
+    void Generate();
+};
+
